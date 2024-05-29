5a7549d657356b0a21a0670d7a9c123539738145
sylim <sylim@danbiedu.co.kr>
2024-05-29 16:36:10 +0900

    initialize project

---
+++ src/test/java/com/task/codetest/CodetestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.codetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodetestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
